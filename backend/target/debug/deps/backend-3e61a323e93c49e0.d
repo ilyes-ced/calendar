70fb7971375c6b0d333a1e8b77f2e7000a5281d5
ilyes-guy <random_dude_233@proton.me>
2023-03-22 22:01:38 +0100

    init

---
+++ backend/target/debug/deps/backend-3e61a323e93c49e0.d
@@ -0,0 +1,5 @@
+/home/ilyes/Documents/projects/rust/to_do_rust/backend/target/debug/deps/backend-3e61a323e93c49e0.rmeta: src/main.rs
+
+/home/ilyes/Documents/projects/rust/to_do_rust/backend/target/debug/deps/backend-3e61a323e93c49e0.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/backend-cc16e90c8bbfbf45.d
@@ -0,0 +1,5 @@
+/home/ilyes/Documents/projects/rust/to_do_rust/backend/target/debug/deps/backend-cc16e90c8bbfbf45.rmeta: src/main.rs
+
+/home/ilyes/Documents/projects/rust/to_do_rust/backend/target/debug/deps/backend-cc16e90c8bbfbf45.d: src/main.rs
+
+src/main.rs:
