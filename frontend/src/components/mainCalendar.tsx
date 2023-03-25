9aeed40713ac84e9295920f0a2b3bb9b0f33b342
ilyes-guy <random_dude_233@proton.me>
2023-03-25 10:52:47 +0100

    dates data

--- frontend/src/components/mainCalendar.tsx
+++ frontend/src/components/mainCalendar.tsx
@@ -1,6 +1,58 @@
 import '../styles/main_calendary.scss'
 
-const arr = [1, 2, 3, 4 , 5,56,5,5,5,5,5,5,5,5,54 , 5,56,5,5,5,5,5,5,5,5,54 , 5,56,5,5,5,5,5,5,5]
+let calendary_days1: [number];
+let calendary_days2: [number];
+let calendary_days3: [number];
+let calendary_days4: [number];
+let calendary_days5: [number];
+let calendary_days6: [number];
+let calendary_days7: [number];
+let calendary_days8: [number];
+let calendary_days9: [number];
+let calendary_days10: [number];
+let calendary_days11: [number];
+let calendary_days12: [number];
+let calendary_days = [
+    [], [], [], [], [], [], [], [], [], [], [], []
+]
+
+function fill_calendar(year: number){
+    var date = new Date(year, 0, 1);
+    var month = date.getMonth();
+    console.log(date, month)
+    while (date.getMonth() === month) {
+        var day: number = date.getDate()
+        calendary_days[month].push(day);
+        date.setDate(date.getDate() + 1);
+    }
+}
+
+
+var today = new Date();
+
+var dd = String(today.getDate()).padStart(2, '0');
+var mm = String(today.getMonth());
+var mm2 = String(today.getMonth() + 1).padStart(2, '0');
+var yyyy = today.getFullYear();
+
+fill_calendar(yyyy)
+console.log(mm);
+console.log(mm2);
+
+
+//var dd = String(today.getDate()).padStart(2, '0');
+//var mm = String(today.getMonth() + 1).padStart(2, '0');
+//var yyyy = today.getFullYear();
+//let stoday = mm + '/' + dd + '/' + yyyy;
+//console.log(yyyy);
+//
+//today.setDate( today.getDate() - 6 );
+//today.setFullYear( today.getFullYear() - 1 );
+//console.log((today.getMonth() ) + '/' + (today.getDate()) + '/' + (today.getFullYear()));
+//stoday = mm + '/' + dd + '/' + yyyy;
+//console.log(stoday);
+//
+
 const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat']
 function App() {
 
@@ -10,16 +62,16 @@ function App() {
 
             </div>
             <div id='calendar_items'>
-                {
+                {/*
                     days.map((ele, i) => {
                         return <div className="day_item"> {ele} </div>
                     })
                 }
                 {
-                    arr.map((ele, i) => {
+                    calendary_days.map((ele, i) => {
                         return <div className="calendar_item">ele</div>
                     })
-                }
+                */}
             </div>
         </div>
     )
