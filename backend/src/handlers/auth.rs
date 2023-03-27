69573a6e437260e11d07c8b77fab3630f54204a0
ilyes-guy <random_dude_233@proton.me>
2023-03-28 00:25:57 +0200

    nothing in p...

--- backend/src/handlers/auth.rs
+++ backend/src/handlers/auth.rs
@@ -20,22 +20,21 @@ async fn login(info: web::Json<User>) -> Result<String> {
 
 
 #[post("/register")]
-async fn register(client: web::Data<Client>, form: web::Form<User>) -> HttpResponse {
+async fn register(client: web::Data<Client>, data: web::Json<User>) -> HttpResponse {
    //println!(format!("Welcome {:?}!", client));
    //println!(format!("Welcome {:?}!", form));
    
 
-   println!("Welcome {:?}!", form);
-   println!("Welcome {:?}!", client);
+   println!("Welcome {:?}!", data);
 
 
    let collection = client.database("rust").collection("users");
-   let result = collection.insert_one(form.into_inner(), None).await;
+   let result = collection.insert_one(data.into_inner(), None).await;
    match result {
       Ok(_) => HttpResponse::Ok().body("user added"),
       Err(err) => HttpResponse::InternalServerError().body(err.to_string()),
    }
-
+   
 }
 
 
--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,4 +1,5 @@
 use actix_web::{web, App, HttpResponse, HttpServer, error};
+use mongodb::{bson::doc, options::IndexOptions, Client, Collection, IndexModel};
 
 pub mod handlers;
 pub mod models;
@@ -11,14 +12,18 @@ async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
 
-    HttpServer::new(|| {
-        let json_config = web::JsonConfig::default()
-        .limit(4096)
-        .error_handler(|err, _req| {
-            error::InternalError::from_response(err, HttpResponse::Conflict().finish())
-                .into()
-        });
+    let uri = std::env::var("MONGODB_URI").unwrap_or_else(|_| "mongodb://localhost:27017".into());
+    let client = Client::with_uri_str(uri).await.expect("failed to connect");
+
+    HttpServer::new(move || {
+        //let json_config = web::JsonConfig::default()
+        //.limit(4096)
+        //.error_handler(|err, _req| {
+        //    error::InternalError::from_response(err, HttpResponse::Conflict().finish())
+        //        .into()
+        //});
         App::new()
+            .app_data(web::Data::new(client.clone()))
             .service(login)
             .service(register)
             //.route("/hey", web::get().to(handlers::manual_hello))
