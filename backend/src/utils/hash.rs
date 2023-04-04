a5700acc47faf374af1c689aa72cdeb6a1616e0a
ilyes-guy <random_dude_233@proton.me>
2023-04-04 23:02:22 +0100

    some ui

--- backend/src/utils/hash.rs
+++ backend/src/utils/hash.rs
@@ -57,21 +57,11 @@ pub fn hash(password: String) -> String  {
     let password = password.as_bytes();
     let salt = SaltString::generate(&mut OsRng);
     
-    // Argon2 with default params (Argon2id v19)
     let argon2 = Argon2::default();
     
-    // Hash password to PHC string ($argon2id$v=19$...)
     let password_hash = argon2.hash_password(password, &salt).unwrap().to_string();
+
     password_hash
-    // Verify password against PHC string.
-    //
-    // NOTE: hash params from `parsed_hash` are used instead of what is configured in the
-    // `Argon2` instance.
-
-    //let parsed_hash = PasswordHash::new(&password_hash).unwrap();
-    //assert!(Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    
-
 
 }
 
@@ -84,12 +74,10 @@ pub fn verify_hash(hash: String, password: String) -> bool {
     println!("{}", parsed_hash);
 
     println!("{}", Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    println!("{}", Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    println!("{}", Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    println!("{}", Argon2::default().verify_password(password, &parsed_hash).is_ok());
-    println!("{}", Argon2::default().verify_password(password, &parsed_hash).is_ok());
 
-    true
+
+    Argon2::default().verify_password(password, &parsed_hash).is_ok()
+
 }
 
 
--- frontend/src/views/home/toDo.tsx
+++ frontend/src/views/home/toDo.tsx
@@ -1,28 +1,40 @@
 import '../../styles/todo.scss'
+import {BsFillPencilFill, BsChevronLeft, BsPlusLg} from  "react-icons/bs"
+
 
 function App() {
 
     return (
         <div id='todo_main' >
             
+           
+           
+
             <div className="category_box">
-                <div className="cat_title">c</div>
+                <div className="cat_header">
+                    <div className='cat_title'>
+                        <div>title</div>
+                        <div>0</div>
+                    </div>
+                    <div className='cat_icons'>
+                        <BsFillPencilFill />
+                        <BsChevronLeft />
+                        <BsPlusLg />
+                    </div>
+                </div>
+
+                <div className='task'>
+                    <div>tyask name</div>
+                    <div className='add_sub_task'>+ add sub task</div>
+                    <div className='icons_bar'>
+                        
+                    </div>
+                </div>
+            
             </div>
-            <div className="category_box">
-                <div className="cat_title">c</div>
-            </div>
-            <div className="category_box">
-                <div className="cat_title">c</div>
-            </div>
-            <div className="category_box">
-                <div className="cat_title">c</div>
-            </div>
-            <div className="category_box">
-                <div className="cat_title">c</div>
-            </div>
-            <div className="category_box">
-                <div className="cat_title">c</div>
-            </div>
+
+       
+       
 
         </div>
     )
