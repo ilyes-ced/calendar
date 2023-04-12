0f9e3c0e89ef65b843657ebe9d09a7f31873b441
ilyes-guy <random_dude_233@proton.me>
2023-04-12 22:19:32 +0200

    login page

--- frontend/src/views/auth/register.tsx
+++ frontend/src/views/auth/register.tsx
@@ -1,11 +1,40 @@
-import { getSearchParamsForLocation } from "react-router-dom/dist/dom"
-
 function App() {
 
     return (
-        <div className="App">
-            hello register
-        </div>
+        <div className="grid align__item">
+
+            <div className="register">
+
+              <svg xmlns="http://www.w3.org/2000/svg" className="site__logo" width="56" height="84" viewBox="77.7 214.9 274.7 412"><defs><linearGradient id="a" x1="0%" y1="0%" y2="0%"><stop offset="0%" stop-color="#6d4aff"/><stop offset="100%" stop-color="#4f27eb"/></linearGradient></defs><path fill="url(#a)" d="M215 214.9c-83.6 123.5-137.3 200.8-137.3 275.9 0 75.2 61.4 136.1 137.3 136.1s137.3-60.9 137.3-136.1c0-75.1-53.7-152.4-137.3-275.9z"/></svg>
+
+              <h2>Sign Up</h2>
+
+              <form action="" method="post" className="form">
+
+                <div className="form__field">
+                  <input type="text" placeholder="username" />
+                </div>
+
+                <div className="form__field">
+                  <input type="email" placeholder="info@mailaddress.com" />
+                </div>
+
+                <div className="form__field">
+                  <input type="password" placeholder="••••••••••••" />
+                </div>
+
+                <div className="form__field">
+                  <input type="submit" value="Sign Up" />
+                </div>
+
+              </form>
+
+              <p>Already have an accout? <a href="#">Log in</a></p>
+
+            </div>
+
+    </div>
+
     )
 }
 
